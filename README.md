b4eaafc7a8eee8bc54cd4c567e09c19d9a8a0294
Doug K Na <dougkna@gmail.com>
2017-05-20 13:26:42 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Quick nginx load balancer demo
+
+### Specifications
+
+1. npm start to fork workers.
+2. run nginx using nginx_config file.
+3. visit localhost:2222 to see load balancing in action.
+
+
---
+++ index.js
@@ -0,0 +1,36 @@
+var cluster = require('cluster'),
+	express = require('express')
+  app = express(),
+  numCPUs = require('os').cpus().length,
+  MasterPort = process.env.PORT || 2121;
+
+if (cluster.isMaster && numCPUs > 1) {
+	var pidToPort = {};
+	var worker, port;
+  for (var i = 0; i < numCPUs; i++) {
+  	port = 8000 + i;
+  	worker = cluster.fork({port: port});
+  	pidToPort[worker.process.pid] = port;
+  }
+
+  app.get('/', function (req, res) {
+    res.end('Welcome to Master page.');
+	});
+
+  app.listen(MasterPort, () => {
+  	console.log('MASTER listening to port %s...', MasterPort);
+  });
+  
+} else {
+	app.set('view engine', 'ejs');
+
+	app.get('/', function (req, res) {
+    res.render('home', {pid: process.pid, port: process.env.port});
+	});
+
+  app.listen(process.env.port, () => {
+  	console.log(process.pid + ' listening to port %s...', process.env.port);
+  });
+}
+
+
