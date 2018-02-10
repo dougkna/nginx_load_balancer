007f02bf0f2eff1fb99501e6b2e8519e745e430f
Doug K Na <dougkna@gmail.com>
2018-02-09 22:08:51 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
 var cluster = require('cluster'),
-  express = require('express'),
-  app = express(),
-  numCPUs = require('os').cpus().length,
-  MasterPort = process.env.PORT || 2121;
+	express = require('express'),
+	app = express(),
+	numCPUs = require('os').cpus().length,
+	masterPort = process.env.PORT || 2121;
 
 if (cluster.isMaster && numCPUs > 1) {
 	var pidToPort = {};
 	var worker, port;
-  for (var i = 0; i < numCPUs; i++) {
-  	port = 8000 + i;
-  	worker = cluster.fork({port: port});
-  	pidToPort[worker.process.pid] = port;
-  }
-  app.get('/', function (req, res) {
-    res.end('Welcome to Master page.');
+	for (var i = 0; i < numCPUs; i++) {
+		port = 8000 + i;
+		worker = cluster.fork({port: port});
+		pidToPort[worker.process.pid] = port;
+	}
+	app.get('/', function (req, res) {
+		res.end('Welcome to Master page.');
 	});
-  app.listen(MasterPort, () => {
-  	console.log('MASTER listening to port %s...', MasterPort);
-  });
+	app.listen(masterPort, () => {
+		console.log('MASTER listening to port %s...', masterPort);
+	});
 } else {
 	app.set('view engine', 'ejs');
 	app.get('/', function (req, res) {
-    res.render('home', {pid: process.pid, port: process.env.port});
+		res.render('home', {pid: process.pid, port: process.env.port});
 	});
-  app.listen(process.env.port, () => {
-  	console.log(process.pid + ' listening to port %s...', process.env.port);
-  });
+	app.listen(process.env.port, () => {
+		console.log(process.pid + ' listening to port %s...', process.env.port);
+	});
 }
-
-
